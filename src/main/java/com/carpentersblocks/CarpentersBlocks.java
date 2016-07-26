c7857f1ac01998296b443eabd78b54486da6e409
Logan <kmartshopper@gmail.com>
2016-07-25 22:06:18 -0700

    Swap in 1.10.2 test.

--- src/main/java/com/carpentersblocks/CarpentersBlocks.java
+++ src/main/java/com/carpentersblocks/CarpentersBlocks.java
@@ -1,54 +1,25 @@
 package com.carpentersblocks;
 
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraftforge.common.config.Configuration;
 import com.carpentersblocks.proxy.CommonProxy;
-import com.carpentersblocks.util.CarpentersBlocksTab;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.network.FMLEventChannel;
-import cpw.mods.fml.common.network.NetworkRegistry;
 
-@Mod(
-        modid = CarpentersBlocks.MODID,
-        name = "Carpenter's Blocks",
-        version = "@VERSION@",
-        dependencies = "required-after:Forge@[10.13.0.1180,)"
-        )
-public class CarpentersBlocks {
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-    public static final String MODID = "CarpentersBlocks";
-    public static FMLEventChannel channel;
-    public static CreativeTabs creativeTab = new CarpentersBlocksTab(MODID);
-
-    @Instance(MODID)
-    public static CarpentersBlocks instance;
-
+@Mod(modid = CarpentersBlocks.MODID, version = CarpentersBlocks.VERSION)
+public class CarpentersBlocks
+{
+    public static final String MODID = "carpentersblocks";
+    public static final String VERSION = "4.0";
+    
     @SidedProxy(clientSide = "com.carpentersblocks.proxy.ClientProxy", serverSide = "com.carpentersblocks.proxy.CommonProxy")
     public static CommonProxy proxy;
-
+    
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-        channel = NetworkRegistry.INSTANCE.newEventDrivenChannel(MODID);
-        Configuration config = new Configuration(event.getSuggestedConfigurationFile());
-        config.load();
-
-        proxy.preInit(event, config);
-
-        if (config.hasChanged()) {
-            config.save();
-        }
+        proxy.preInit(event);
     }
-
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        proxy.init(event);
-    }
-
+    
 }
